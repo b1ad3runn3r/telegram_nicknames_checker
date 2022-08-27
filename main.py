389502d4cb3332a1651bccfd586dd82124afa1b1
amd64 <mihk4431@gmail.com>
2022-08-27 21:55:58 +0300

    Create bot command handlers
    This bot can now handle such commands as /start and /help

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import dotenv_values
 import asyncio
 from telethon import TelegramClient, events
 
+
 config = dotenv_values('.env')
 
 bot_token = config['BOT_TOKEN']
@@ -11,15 +12,25 @@ api_hash = config['API_HASH']
 bot = TelegramClient('bot', api_id, api_hash)
 
 
-@bot.on(events.NewMessage)
-async def test_event_handler(event):
-    if 'hello' in event.raw_text:
-        await event.reply('hi!')
+@bot.on(events.NewMessage(incoming=True, pattern="/start"))
+async def start(event):
+    sender = await event.get_sender()
+    msg = f"Hello, **{sender.username}**!\
+            \nThis is a username checker bot.\
+            \nPlease send me a **.csv** file and I will check."
+
+    await event.reply(msg)
 
 
-@bot.on(events.NewMessage)
-async def get_file_size_handler(event):
-    
+@bot.on(events.NewMessage(incoming=True, pattern="/help"))
+async def bot_help(event):
+    msg = f"/start - begin interaction with bot\
+            \n/help - print this help message\
+            \nHow to use this bot:\
+            \nSend a **.csv** file and it will check the list of usernames for availability."
+
+    await event.reply(msg)
+
 
 with bot:
     bot.start(bot_token=bot_token)
